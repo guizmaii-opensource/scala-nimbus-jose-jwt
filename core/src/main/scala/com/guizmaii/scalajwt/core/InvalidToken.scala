b31e8087265ac336bceeb49c41d15efb1bae46c4
Jules Ivanic <jules.ivanic@gmail.com>
2025-12-03 21:22:31 +1100

    Mark the nullable cause as "unsafe" (#490)

--- core/src/main/scala/com/guizmaii/scalajwt/core/InvalidToken.scala
+++ core/src/main/scala/com/guizmaii/scalajwt/core/InvalidToken.scala
@@ -1,6 +1,9 @@
 package com.guizmaii.scalajwt.core
 
-final case class InvalidToken(message: String, cause: Throwable = null) extends RuntimeException(message, cause)
+final case class InvalidToken(message: String, unsafeCause: Throwable | Null = null) extends RuntimeException(message, unsafeCause) {
+  override def getCause: Throwable = unsafeCause
+  def cause: Option[Throwable]     = Option(unsafeCause)
+}
 object InvalidToken {
-  def apply(t: Throwable): InvalidToken = InvalidToken(message = t.getMessage, cause = t)
+  def apply(t: Throwable): InvalidToken = InvalidToken(message = t.getMessage, unsafeCause = t)
 }
