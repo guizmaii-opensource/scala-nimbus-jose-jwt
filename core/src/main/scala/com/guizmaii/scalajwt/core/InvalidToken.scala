e4cbbde1a7ba39c29a1dfed752e31d0f45bae60e
Jules Ivanic <jules.ivanic@gmail.com>
2025-12-03 20:58:44 +1100

    Clean code

--- core/src/main/scala/com/guizmaii/scalajwt/core/InvalidToken.scala
+++ core/src/main/scala/com/guizmaii/scalajwt/core/InvalidToken.scala
@@ -1,3 +1,6 @@
 package com.guizmaii.scalajwt.core
 
-final case class InvalidToken(cause: Throwable) extends RuntimeException(cause.getMessage, cause)
+final case class InvalidToken(message: String, cause: Throwable = null) extends RuntimeException(message, cause)
+object InvalidToken {
+  def apply(t: Throwable): InvalidToken = InvalidToken(message = t.getMessage, cause = t)
+}
